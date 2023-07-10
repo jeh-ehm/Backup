d87fe170e0a6a524691806f5bb995333330e9247
jeh-ehm <jm.serdena24@gmail.com>
2023-07-11 00:03:01 +0800

    add slide in and out of the incident module (rest are not implemented)

--- src/app/incident/incident.page.ts
+++ src/app/incident/incident.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AnimationController, Animation } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-incident',
@@ -10,17 +12,40 @@ export class IncidentPage implements OnInit {
 
   isToastOpen = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private animationCtrl: AnimationController, private elementRef: ElementRef) { }
 
   ngOnInit() {
   }
 
   goBack() {
-    this.router.navigate(['./home-screen']);
+    this.show("0", "100%", 1 , 0);
+    setTimeout(() => {
+      this.router.navigate(['./home-screen']);
+    }, 350);
   }
 
   setOpen(isOpen: boolean) {
     this.isToastOpen = isOpen;
   }
 
+  ionViewDidEnter() {
+    this.show("100%", "0", 0, 1);
+  }
+
+  show(upVal: string, downVal: string, upOpac: number, downOpac: number) {
+    const element = this.elementRef.nativeElement.querySelector('.animate-on-load');
+    if (element) {
+      const animation: Animation = this.animationCtrl
+        .create()
+        .addElement(element)
+        .duration(400)
+        .easing('ease-in-out')
+        .keyframes([
+          { offset: 0, opacity: upOpac, transform: 'translateY(' + upVal + ')' },
+          { offset: 1, opacity: downOpac, transform: 'translateY(' + downVal + ')' },
+        ]);
+
+      animation.play();
+    }
+  }
 }
